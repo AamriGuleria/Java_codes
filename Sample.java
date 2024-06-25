e138f081046677fb6be759951fbe14f71919ff78
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2024-06-25 11:48:01 +0530

    Day 8

---
+++ Sample.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Sample {
+    public static void main(String[] args) {
+        ArrayList<String> arr=new ArrayList<>();
+        arr.add("dba");
+        arr.add("dca");
+        Collections.sort(arr);
+        System.out.println(arr);
+    }
+}
