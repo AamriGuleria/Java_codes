44c880c9925b883b63594324d8213ca408f99a4d
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2024-06-28 15:33:18 +0530

    Day 10

---
+++ Dp/Triangle.java
@@ -0,0 +1,15 @@
+package Dp;
+
+import java.util.List;
+
+public class Triangle {
+    public static void helper(List<List<Integer>> triangle){
+        for(int i=triangle.size()-2;i>=0;i--){
+            for(int j=0;j<triangle.get(i).size();j++){
+                int minvalue=java.lang.Math.min(triangle.get(i+1).get(j),triangle.get(i+1).get(j+1));
+                triangle.get(i).set(j,triangle.get(i).get(j)+minvalue);
+            }
+        }
+        return triangle.get(0).get(0);
+    }
+}
