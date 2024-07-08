4b29d18a68c41f71efb688fd950f57b0a8a8e711
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2024-07-08 17:06:48 +0530

    Day 17

---
+++ Dp/UniqueBST.java
@@ -0,0 +1,13 @@
+public class UniqueBST {
+    public static void main(String[] args) {
+        int n=3;
+        int[] dp=new int[n+1];
+        dp[0]=dp[1]=1;
+        for(int i=2;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                dp[i]+=dp[j-1]*dp[i-j];
+            }
+        }
+        System.out.println(dp[n]);
+    }
+}
