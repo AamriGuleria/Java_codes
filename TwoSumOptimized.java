c5c0f2b559c60ffc3613ebde78fa6a195d37aea7
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2024-07-09 11:02:33 +0530

    Day 18

---
+++ TwoSumOptimized.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+
+public class TwoSumOptimized {
+    public static void main(String[] args) {
+        
+    }
+    public static int[] check(int[] nums,int target){
+        HashMap<Integer,Integer> hm=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int complement=target-nums[i];
+            if(hm.containsKey(complement)){
+                return new int[]{i,hm.get(complement)};
+            }
+            else{
+                hm.put(nums[i],i);
+            }
+        }
+        return new int[]{};
+    }
+}
