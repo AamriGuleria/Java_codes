d248050c823f07d6d009b28befe2f853b0b2b838
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2024-07-09 13:48:39 +0530

    Day 18

--- ContainsDuplicateBF.java
+++ ContainsDuplicateBF.java
@@ -1,9 +1,19 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ContainsDuplicateBF {
     public static void main(String[] args) {
         int[] nums={1,1,1,3,3,4,3,2,4,2};
 
+    }
+    public static boolean check2(int[] nums){
+        Arrays.sort(nums);
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]==nums[i-1]){
+                return true;
+            }
+        }
+        return false;
     }
     public static boolean check(int[] nums){
         ArrayList<Integer> arr=new ArrayList<>();
