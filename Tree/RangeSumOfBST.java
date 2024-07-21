689ccb9284d4d879a7728572ac1a27590c21cb8e
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2024-07-21 14:40:36 +0530

    Tree question

--- Tree/RangeSumOfBST.java
+++ Tree/RangeSumOfBST.java
@@ -1,6 +1,18 @@
 package Tree;
 
 public class RangeSumOfBST {
+     public class TreeNode {
+             int val;
+             TreeNode left;
+             TreeNode right;
+             TreeNode() {}
+             TreeNode(int val) { this.val = val; }
+             TreeNode(int val, TreeNode left, TreeNode right) {
+                 this.val = val;
+                 this.left = left;
+                 this.right = right;
+             }
+         }
     public int rangeSumBST(TreeNode root, int low, int high) {
         if(root==null){
             return 0;
