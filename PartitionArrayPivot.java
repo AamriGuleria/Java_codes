0d6e23d8e8e6377f02630b7305355bbd81e9e515
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2024-10-03 23:11:58 +0530

    optimized approach

--- PartitionArrayPivot.java
+++ PartitionArrayPivot.java
@@ -23,4 +23,34 @@ public class PartitionArrayPivot {
         }
         return relIndex;
     }
+    public int[] pivotArrayOptimized(int[] nums, int pivot) {
+        int n=nums.length;
+        int[] result=new int[n];
+        int smaller=0;
+        int pivotCount=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]<pivot){
+                smaller++;
+            }
+            else if(nums[i]==pivot){
+                pivotCount++;
+            }
+        }
+        int larger=n-(pivotCount+smaller);
+        int lessIndex=0;
+        int pivotIndex=smaller;
+        int greaterIndex=smaller+pivotCount;
+        for(int num:nums){
+            if(num<pivot){
+                result[lessIndex++]=num;
+            }
+            else if(num==pivot){
+                result[pivotIndex++]=num;
+            }
+            else{
+                result[greaterIndex++]=num;
+            }
+        }
+        return result;
+    }
 }
