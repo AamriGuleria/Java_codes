07316bfa12e37f1e04efe198020230fd445e9afd
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2024-10-08 18:23:03 +0530

    Brute force

--- LongerContiguousSegmentsofOnesthanZeros.java
+++ LongerContiguousSegmentsofOnesthanZeros.java
@@ -1,9 +1,31 @@
 public class LongerContiguousSegmentsofOnesthanZeros {
+    public boolean checkZeroOnesOptimized(String s) {
+        int maxZeroes=0;
+        int currZeroes=0;
+        int currOnes=0;
+        int maxOnes=0;
+        int n=s.length();
+        for(int i=0;i<n;i++){
+            int ch=s.charAt(i);
+            if(ch=='0'){
+                currZeroes++;
+                currOnes=0;
+            }
+            else{
+                currOnes++;
+                currZeroes=0;
+            }
+            maxZeroes = Math.max(maxZeroes, currZeroes);
+            maxOnes = Math.max(maxOnes, currOnes);
+        }
+        return maxOnes>maxZeroes;
+    }
     public boolean checkZeroOnes(String s) {
         int zeroes=countZeroes(s);
         int ones=countOnes(s);
         return ones > zeroes;
     }
+
     public static int countZeroes(String s){
         int max=0;
         int tmp=0;
